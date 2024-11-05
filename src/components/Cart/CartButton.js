dda6365e50dd1c89720f06198369197799450356
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-11-05 14:52:02 +0700

    Make cartButton show the current cartItems length

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,13 +1,15 @@
 import classes from "./CartButton.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/cart";
 const CartButton = (props) => {
   const dispatcher = useDispatch();
   const handleToggleModal = () => dispatcher(cartActions.toggle());
+
+  const cartItemsLength = useSelector((state) => state.cart.cartItems.length);
   return (
     <button className={classes.button} onClick={handleToggleModal}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartItemsLength}</span>
     </button>
   );
 };
