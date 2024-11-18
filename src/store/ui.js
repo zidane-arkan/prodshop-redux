779844b97b316293a3cec0d8d51e3a4ba98207f8
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-11-18 10:22:21 +0700

    Add reducers for notifications

--- src/store/ui.js
+++ src/store/ui.js
@@ -1,12 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUiSlice = { isCartVisible: false };
+const initialUiSlice = {
+  isCartVisible: false,
+  notificationStatus: null,
+};
 const uiSlice = createSlice({
   name: "ui",
   initialState: initialUiSlice,
   reducers: {
     toggleCart(state) {
       state.isCartVisible = !state.isCartVisible;
+    },
+    handleNotification(state, action) {
+      state.notificationStatus = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
